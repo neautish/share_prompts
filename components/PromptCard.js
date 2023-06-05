b8817125f66b9e66c068a21826e4fdc1d72165d0
navab rahimi <neautish.1371@gmail.com>
2023-06-05 13:10:47 +0330

    fix a bug

--- components/PromptCard.js
+++ components/PromptCard.js
@@ -20,15 +20,15 @@ function PromptCard({ post, handleTagClick }) {
 		<div className="prompt_card">
 			<div className="flex justify-between items-start gap-5">
 				<div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-					<Image src={post.creator.image} alt="user" width={40} height={40} className="rounded-full object-contain" />
+					<Image src={post?.creator?.image} alt="user" width={40} height={40} className="rounded-full object-contain" />
 				</div>
 
 				<div className="flex flex-col">
-					<h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
-					<p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
+					<h3 className="font-satoshi font-semibold text-gray-900">{post?.creator?.username}</h3>
+					<p className="font-inter text-sm text-gray-500">{post?.creator?.email}</p>
 				</div>
 				<div className="copy_btn" onClick={handleCopy}>
-					<Image src={copied === post.prompt ? "/assets/icons/tick.svg" : "/assets/icons/copy.svg"} width={12} height={12} />
+					<Image alt="copy" src={copied === post.prompt ? "/assets/icons/tick.svg" : "/assets/icons/copy.svg"} width={12} height={12} />
 				</div>
 			</div>
 			<p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
