190e399360a65a0f458da1f7dfe402dea479c595
navab rahimi <neautish.1371@gmail.com>
2023-06-06 12:01:15 +0330

    add profile section and edit and delete funcionalities

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,7 +1,51 @@
-import React from "react";
+"use client";
 
-function Profile() {
-	return <div>Profile</div>;
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+function MyProfile() {
+	const router = useRouter();
+	const { data: session } = useSession();
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch(`/api/users/${session?.user.id}/posts`);
+			const data = await response.json();
+
+			setPosts(data);
+		};
+
+		if (session?.user.id) {
+			fetchPosts();
+		}
+	}, [session]);
+
+	const handleEdit = (post) => {
+		router.push(`/update-prompt?id=${post._id}`);
+	};
+	const handleDelete = async (post) => {
+		const hasConfirmed = confirm("Are you sure?");
+		if (hasConfirmed) {
+			try {
+				await fetch(`/api/prompt/${post._id.toString()}`, {
+					method: "DELETE",
+				});
+
+				const filteredPosts = posts.filter((p) => p._id !== post._id);
+				setPosts(filteredPosts);
+
+				return new Response("Prompt deleted!");
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	};
+
+	return <Profile name="My" desc="Welcome to my profile." data={posts} handleEdit={handleEdit} handleDelete={handleDelete} />;
 }
 
-export default Profile;
+export default MyProfile;
--- components/PromptCard.js
+++ components/PromptCard.js
@@ -5,7 +5,9 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-function PromptCard({ post, handleTagClick }) {
+function PromptCard({ post, handleTagClick, handleEdit, handleDelete }) {
+	const { data: session } = useSession();
+	const pathname = usePathname();
 	const [copied, setCopied] = useState("");
 
 	const handleCopy = () => {
@@ -33,8 +35,19 @@ function PromptCard({ post, handleTagClick }) {
 			</div>
 			<p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
 			<p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(post.tag)}>
-				{post.tag}
+				#{post.tag}
 			</p>
+
+			{session?.user.id === post.creator._id && pathname === "/profile" && (
+				<div className="mt-5 flex-center gap-4 border-t border-gray-200 pt-3">
+					<p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
+						Edit
+					</p>
+					<p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>
+						Delete
+					</p>
+				</div>
+			)}
 		</div>
 	);
 }
