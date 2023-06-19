470ab7593f24dbab302555a2527bc912e0ae2e4e
navab rahimi <neautish.1371@gmail.com>
2023-06-19 12:21:54 +0330

    deleted some commented code

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,15 +4,9 @@ import { connectToDB } from "@utils/database";
 export const revalidate = 0;
 
 export const GET = async (request) => {
-	// const url = new URL(request.url);
-	// const searchParams = url.searchParams;
-	// const search = searchParams.get("search") || "";
-	// const regex = new RegExp(search, "i");
-
 	try {
 		await connectToDB();
 
-		// const prompts = await Prompt.find({ prompt: { $regex: regex } }).populate("creator");
 		const prompts = await Prompt.find().populate("creator");
 
 		return new Response(JSON.stringify(prompts), { status: 200 });
